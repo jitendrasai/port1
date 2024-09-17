e0018e59d3fd6e170c9153a2b2bec8476615fe71
jitendrasai <jitendrasai333@gmail.com>
2024-09-16 20:54:58 -0400

    image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,8 +11,6 @@ export const Hero = () => {
         <p className={styles.description}>
         An analytical thinker with a strong background in data science, including expertise in Python, SQL, and data visualization tools.
         With one year of experience in applying machine learning algorithms, statistical analysis, and data preprocessing,
-         I am currently pursuing a master's degree in CSE at Florida State University.
-        Eagerly waiting to leverage my skills in a dynamic role to drive data-driven decision-making and contribute to impactful projects.
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
